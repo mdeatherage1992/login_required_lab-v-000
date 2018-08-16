867f6391809cada5a6385da2a2d602147fad741a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-16 16:04:31 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,6 +1,9 @@
 class SecretsController < ApplicationController
    before_action :require_login
 
+   def show
+   end 
+
   def require_login
     return head(:forbidden) unless session.include? :user_id
   end
