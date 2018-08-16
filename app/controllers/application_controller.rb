da3cf98988ff3c0f8bd45d874aa12a1bae4eec0a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-08-16 15:52:36 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,9 @@ class ApplicationController < ActionController::Base
   def current_user
    session[:name]
  end
- 
+
 private
+
  def require_logged_in
   redirect_to controller: 'sessions', action: 'new' unless current_user
 end
